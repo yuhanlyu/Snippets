778f7dfbb8f0e2738b466110727c82012bee9a95
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2012-11-19 21:17:41 -0500

    Greatest Common Divisor

--- gcd.c
+++ gcd.c
@@ -15,5 +15,6 @@ int main( void )
 {
     printf( "GCD of 5 and 7 should be 1. gcd(5, 7) = %u\n", gcd( 5, 7 ) );
     printf( "GCD of 54 and 24 should be 6. gcd(54, 24) = %u\n", gcd( 54, 24 ) );
+
     return 0;
 }
