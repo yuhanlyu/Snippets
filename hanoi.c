7fdbde18812ae71275d81146506ec75c8150bf16
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2012-11-20 20:05:51 -0500

    Tower of Hanoi

--- hanoi.c
+++ hanoi.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/** Computes the solution of tower of Hanoi with n disks */
-void hanoi( unsigned int n );
-void hanoi( unsigned int n )
+/** Computes the solution of tower of Hanoi for given numberOfDisks */
+void hanoi( unsigned int numberOfDisks );
+void hanoi( unsigned int numberOfDisks )
 {
     /* Compute the solution by using binary representation */
-    for ( int x = 1; x < (1 << n); ++x ) {
+    for ( int x = 1; x < (1 << numberOfDisks); ++x ) {
         printf( "Move from peg %d to peg %d.\n", 
             (x & (x-1)) % 3, ((x | (x-1)) + 1) % 3 );
     }
