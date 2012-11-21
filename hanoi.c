f0d520289036fb06d9249ee4bb5682271937177f
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2012-11-20 19:09:36 -0500

    Tower of Hanoi

---
+++ hanoi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/** Computes the solution of tower of Hanoi with n disks */
+void hanoi( unsigned int n );
+void hanoi( unsigned int n )
+{
+    for ( int x = 1; x < (1 << n); ++x )
+        printf( "Move from peg %d to peg %d.\n", 
+		          (x & (x-1)) % 3, ((x | (x-1)) + 1) % 3 );
+}
+                                                                                
+int main( void )
+{
+	hanoi( 5 );
+
+    return 0;
+}
