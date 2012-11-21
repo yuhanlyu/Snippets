cd72d250745a333cad2ceaac48b76805e4082cf8
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2012-11-20 19:14:13 -0500

    Tower of Hanoi

--- hanoi.c
+++ hanoi.c
@@ -4,14 +4,16 @@
 void hanoi( unsigned int n );
 void hanoi( unsigned int n )
 {
-    for ( int x = 1; x < (1 << n); ++x )
+    /* Compute the solution by using binary representation */
+    for ( int x = 1; x < (1 << n); ++x ) {
         printf( "Move from peg %d to peg %d.\n", 
-		          (x & (x-1)) % 3, ((x | (x-1)) + 1) % 3 );
+            (x & (x-1)) % 3, ((x | (x-1)) + 1) % 3 );
+    }
 }
                                                                                 
 int main( void )
 {
-	hanoi( 5 );
+    hanoi( 5 );
 
     return 0;
 }
