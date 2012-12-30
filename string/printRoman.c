7f4502555b1e645a638ef7852cef86dc1fbe32a6
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2012-12-29 20:07:41 -0500

    Add printRoman.c

---
+++ string/printRoman.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void printRomanNumber( int decimal );
+void printRomanNumber( int decimal )
+{
+    const int  s[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    const char *output[] = { "M", "CM", "D", "CD", "C", "XC", "L",
+                             "XL", "X", "IX", "V", "IV", "I" };
+    for ( int i = 0; decimal > 0; ++i ) {
+        while ( decimal >= s[ i ] )  {
+            printf( "%s", output[i] );
+            decimal -= s[i];
+        }
+    }
+}
+                                                                                
+int main( void )
+{
+    printf( "1910 should be MCMX: " );
+    printRomanNumber( 1910 );
+    puts( "" );
+    printf( "1954 should be MCMLIV: " );
+    printRomanNumber( 1954 );
+    puts( "" );
+    return 0;
+}
