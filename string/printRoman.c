df1b297c7975c7f5f5ef17dd22a0a5c67abe1440
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2012-12-30 12:45:35 -0500

    Print the Roman numeral of an integer

--- string/printRoman.c
+++ string/printRoman.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 void printRomanNumber( int decimal );
+
+/* Print the roman numeral of decimal */
 void printRomanNumber( int decimal )
 {
     const int  s[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
