d8319b131d85249e2aed53e9a2a115582c7b2302
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2013-01-10 17:30:48 -0500

    String comparison

---
+++ string/strcmp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* Compare two strings */
+int strcmp( const char *s1, const char *s2 );
+
+int strcmp( const char *s1, const char *s2 )
+{
+    for (; *s1 && ( *s1 == *s2 ); ++s1, ++s2)
+        ;
+    // By standard, return value is determined by the sign of the 
+    // difference between the values of the first pair of characters 
+    // (both interpreted as unsigned char) that differ in the objects being compared.
+    return *(const unsigned char *)s1 - *(const unsigned char *)s2;
+}
+
+int main( void )
+{
+    printf( "%d\n", strcmp( "aaa", "aab" ) );
+
+    return 0;
+}
