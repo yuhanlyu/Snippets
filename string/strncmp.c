e0219beb4a2fac1e585d1b8b5945b4c3d8107771
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2013-01-13 09:45:42 -0500

    String comparison for n characters

--- string/strncmp.c
+++ string/strncmp.c
@@ -6,10 +6,9 @@ int strncmp( const char s1[], const char s2[], size_t n )
 {
     /* When n = 0, must return 0 */
     if ( n == 0 ) return 0;
-    for ( ; n-- && *s1 == *s2; ++s1, ++s2 )
-        /* No comparison is made after null character */
-        if ( n == 0 || *s1 == '\0' )
-            return 0;
+    for ( ; n-- && *s1 && *s1 == *s2; ++s1, ++s2 )
+        /* No comparison is made after n characters */
+        if ( n == 0 ) return 0;
     return *(unsigned const char *)s1 - *(unsigned const char *)s2;
 }
 
