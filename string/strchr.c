ceba7fa7a0444c26008a17e8d10aa4b84228a5e5
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2013-01-13 19:25:02 -0500

    Locate the first occurence of a character in the string

--- string/strchr.c
+++ string/strchr.c
@@ -4,6 +4,7 @@
 char *strchr( const char string[], int c);
 char *strchr( const char string[], int c)
 {
+    /* c can be '\0', thus we should compare first */
     while ( *string != (char)c )
         if (!*string++)
             return NULL;
