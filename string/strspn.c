aa7f66b55b8c73fbc2d6c7153e4355dfaf00da44
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2013-01-15 18:25:30 -0500

    Find the longest prefix of a string for a set of characters

---
+++ string/strspn.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+extern char *strchr( const char s1[], int c );
+
+/* Find the longest prefix of s1 that are characters in s2 */
+size_t strspn( const char s1[], const char s2[]);
+size_t strspn( const char s1[], const char s2[])
+{
+    size_t len = 0;
+    while ( *s1 && strchr(s2, *s1++) )
+        ++len;
+    return len;
+}
+
+int main( void )
+{
+    char    a[] = "Hello", b[] = "eH";
+
+    printf( "%zd\n", strspn( a, b ) );
+    return 0;
+}
+
