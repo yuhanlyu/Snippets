98a14d210f2090c2c83f715a61a0ec7b67a4baa4
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2013-01-18 22:16:36 -0500

    Locate the last occurence of a character in the string

--- string/strrchr.c
+++ string/strrchr.c
@@ -16,9 +16,16 @@ char *strrchr( const char string[], int c)
 
 int main( void )
 {
-    char    a[] = "Hello";
-
-    printf( "%c\n", *strrchr( a, 'l' ) );
+    printf( "strrchr( \"\", '\\0' ) should be an empty string: %s\n",
+             strrchr( "", '\0' ) );
+    printf( "strrchr( \"Hello\", '\\0' ) should be an empty string: %s\n",
+             strrchr( "Hello", '\0' ) );
+    printf( "strrchr( \"Hello\", 'o' ) should be o: %s\n",
+             strrchr( "Hello", 'o' ) );
+    printf( "strrchr( \"Hello\", 'H' ) should be Hello: %s\n",
+             strrchr( "Hello", 'H' ) );
+    printf( "strrchr( \"Hello\", 'l' ) should be lo: %s\n",
+             strrchr( "Hello", 'l' ) );
     return 0;
 }
 
