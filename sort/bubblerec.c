ffc57fcb7058938d4f34ae5ebf00181cfb7e37f4
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2013-05-20 13:34:30 -0400

    Bubble sort recursive version

--- sort/bubblerec.c
+++ sort/bubblerec.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
-#define MAX 10
 
+/* Bubble sort */
 void bubble( int a[], int n );
-
 void bubble( int a[], int n )
 {
-    int     i, t;
-
     if ( n == 1 )
         return;
-    for ( i = 0; i < n - 1; ++i )
+    for ( int i = 0; i < n - 1; ++i ) {
         if ( a[ i ] > a[ i + 1 ] ) {
-            t = a[ i ];
+            int t = a[ i ];
             a[ i ] = a[ i + 1 ];
             a[ i + 1 ] = t;
         }
+    }
     bubble( a, n - 1 );
 }
 
 int main( void )
 {
-    int     a[ MAX ] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, i;
+    int     a[ 10 ] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
 
-    bubble( a, MAX );
-    for ( i = 0 ; i < MAX; ++i )
+    bubble( a, 10 );
+    for ( int i = 0 ; i < 10; ++i )
         printf( "%d ", a[ i ] );
     puts( "" );
     return 0;
