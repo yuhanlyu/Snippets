a9d5000a4cf37f1a182dfaf3ffc356657115d1d6
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2013-05-20 13:31:23 -0400

    Bubble Sort

--- sort/bubble.c
+++ sort/bubble.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+/* Bubble sort */
 void bubble( int a[], int n );
+void bubble( int a[], int n )
+{
+    for ( int i = 0; i < n - 1; ++i )
+        for ( int j = n - 1; j > i; --j )
+            if ( a[ j - 1 ] > a[ j ] ) {
+                int t = a[ j ]; 
+                a[ j ] = a[ j - 1 ]; 
+                a[ j - 1 ] = t;
+            }
+}
 
 int main( void )
 {
@@ -11,14 +22,3 @@ int main( void )
         printf( "%d ", a[ i ] );
     return 0;
 }
-
-void bubble( int a[], int n )
-{
-    int     i, j, t;
-
-    for ( i = 0; i < n - 1; ++i )
-        for ( j = n - 1; j > i; --j )
-            if ( a[ j - 1 ] > a[ j ] )
-                t = a[j], a[j] = a[j-1], a[j-1] = t;
-}
-
