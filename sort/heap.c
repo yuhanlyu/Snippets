fcc8933adb5038e27f8edcaf4436e0228da0a9d5
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2013-05-22 13:07:38 -0400

    Heap sort

--- sort/heap.c
+++ sort/heap.c
@@ -3,43 +3,42 @@
 void heapsort( int a[], int n );
 void fixDown( int a[], int k, int n );
 
+/* Heap sort */
+void heapsort( int a[], int n )
+{
+    /* Build a max-heap */
+    for ( int i = n / 2; i >= 0; --i )
+        fixDown( a, i, n );
+    /* Iteratively extract the maximum from the heap*/
+    for ( ; n-- > 1; fixDown( a, 0, n ) ) {
+        int tmp = a[ 0 ];
+        a[ 0 ] = a[ n ];
+        a[ n ] = tmp;
+    }
+}
+
+/* Fix the heap with n elements from the root */
+void fixDown( int a[], int root, int n )
+{
+    /* The possible children of root are 2*root + 1 and 2*root + 2 */
+    for ( int maxchild; ( maxchild = 2 * root + 1 ) < n; root = maxchild ) {
+        if ( maxchild + 1 < n && 
+             a[ maxchild ] < a[ maxchild + 1 ] )
+            ++maxchild;
+        if ( a[ root ] >= a[ maxchild ] )
+            break;
+        int tmp = a[ root ];
+        a[ root ] = a[ maxchild ];
+        a[ maxchild ] = tmp;
+    }
+}
+
 int main( void )
 {
-    int     a[ 6 ] = { 6, 5, 4, 3, 2, 1 }, i;
+    int     a[ 6 ] = { 1, 5, 3, 9, 7, 11 };
 
     heapsort( a, 6 );
-    for ( i = 0; i < 6; ++i )
+    for ( int i = 0; i < 6; ++i )
         printf( "%d ", a[ i ] );
     return 0;
 }
-
-void heapsort( int a[], int n )
-{
-    int     i, tmp;
-
-    for ( --a, i = n / 2; i >= 1; --i )
-        fixDown( a, i, n );
-    while ( n > 1 ) {
-        tmp = a[ 1 ];
-        a[ 1 ] = a[ n ];
-        a[ n ] = tmp;
-        fixDown( a, 1, --n );
-    }
-}
-
-void fixDown( int a[], int k, int n )
-{
-    int     j, tmp;
-
-    while ( 2 * k <= n ) {
-        j = 2 * k;
-        j += ( j < n && a[ j ] < a[ j + 1 ] );
-        if ( a[ k ] >= a[ j ] )
-            break;
-        tmp = a[ k ];
-        a[ k ] = a[ j ];
-        a[ j ] = tmp;
-        k = j;
-    }
-}
-
