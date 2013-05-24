5ed30c17a616eaa456e8cf0cd3a1fb5b0d0280d8
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2013-05-24 14:36:08 -0400

    Insertion sort

--- sort/insertion.c
+++ sort/insertion.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 
+/* Insertion Sort */
 void insertion( int a[], int n );
-
-int main( void )
-{
-    int     a[ 5 ] = { 5, 4, 3, 2, 1 }, i;
-
-    insertion( a, 5 );
-    for ( i = 0; i < 5; ++i )
-        printf( "%d ", a[ i ] );
-    return 0;
-}
-
 void insertion( int a[], int n )
 {
-    int     i, j, tmp;
-
-    for ( i = 1; i < n; ++i ) {
-        for ( tmp = a[ i ], j = i; j > 0 && a[ j - 1 ] > tmp; --j )
+    for ( int i = 1; i < n; ++i ) {
+        int     j, tmp = a[ i ];
+        for ( j = i; j > 0 && a[ j - 1 ] > tmp; --j )
             a[ j ] = a[ j - 1 ];
         a[ j ] = tmp;
     }
 }
 
+int main( void )
+{
+    int     a[ 5 ] = { 5, 4, 3, 2, 1 };
+
+    insertion( a, 5 );
+    for ( int i = 0; i < 5; ++i )
+        printf( "%d ", a[ i ] );
+    return 0;
+}
