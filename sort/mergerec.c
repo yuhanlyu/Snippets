f45d1fcf6df186db66aeb0fd13ce06f543e9c228
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2013-05-27 10:04:06 -0400

    Merge sort recursive version

--- sort/mergerec.c
+++ sort/mergerec.c
@@ -1,44 +1,43 @@
 #include <stdio.h>
 
-void merge( int a[], int left, int mid, int right );
 void mergesort( int a[], int left, int right );
+void merge( int a[], int left, int mid, int right );
 
-int main( void )
-{
-    int     a[ 6 ] = { 6, 5, 4, 3, 2, 1 }, i;
-
-    mergesort( a, 0, 5 );
-    for ( i = 0; i < 6; ++i )
-        printf( "%d ", a[ i ] );
-    return 0;
-}
-
+/* Merge sort: sort the array a from a[left] to a[right - 1] */
 void mergesort( int a[], int left, int right )
 {
-    int     i, m, min;
+    if ( left < right - 1 ) {
+        int     mid = left + ( right - left )/2;
 
-    for ( m = 1; m <= right - left; m += m )
-        for ( i = left; i <= right - m; i += m + m ) {
-            min = right < i + m + m - 1 ? right : i + m + m - 1;
-            merge( a, i, i + m - 1, min );
-        }
+        mergesort( a, left, mid);
+        mergesort( a, mid, right );
+        merge( a, left, mid, right );
+    }
 }
 
+/* Merge: Merge two sorted array a[left .. mid - 1], a[mid .. right - 1] */
 void merge( int a[], int left, int mid, int right )
 {
-    int     aux[ right - left + 1 ], i = left, j = mid + 1, k;
+    int     aux[ right - left ];
 
-    for ( k = 0; k <= right - left; ++k ) {
-        if ( i == mid + 1 ) {
+    for ( int i = left, j = mid, k = 0; k < right - left; ++k ) {
+        if ( i == mid ) {
             aux[ k ] = a[ j++ ];
-            continue;
-        } else if ( j == right + 1 ) {
+        } else if ( j == right ) {
             aux[ k ] = a[ i++ ];
-            continue;
         } else
             aux[ k ] = a[ i ] < a[ j ] ? a[ i++ ] : a[ j++ ];
     }
-    for ( i = 0, j = left; j <= right; ++i, ++j )
+    for ( int i = 0, j = left; j < right; ++i, ++j )
         a[ j ] = aux[ i ];
 }
 
+int main( void )
+{
+    int     a[ 6 ] = { 1, 5, 3, 2, 4, 6 };
+
+    mergesort( a, 0, 6 );
+    for ( int i = 0; i < 6; ++i )
+        printf( "%d ", a[ i ] );
+    return 0;
+}
