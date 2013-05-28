8ba43facb9a926d72b48e671c02d4441b15e03f5
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2013-05-28 06:55:09 -0400

    Selection sort

--- sort/selection.c
+++ sort/selection.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 
+/* Selection sort */
 void selection( int a[], int n );
-
-int main( void )
-{
-    int     a[ 5 ] = { 5, 4, 3, 2, 1 }, i;
-
-    selection( a, 5 );
-    for ( i = 0; i < 5; ++i )
-        printf( "%d ", a[ i ] );
-    return 0;
-}
-
 void selection( int a[], int n )
 {
-    int     i, j, min, tmp;
-
-    for ( i = 0; i < n - 1; ++i ) {
-        for ( min = i, j = i + 1; j < n; ++j )
+    for ( int i = 0, min = 0; i < n - 1; ++i, min = i ) {
+        for ( int j = i + 1; j < n; ++j ) {
             if ( a[ j ] < a[ min ] )
                 min = j;
-        tmp = a[ i ];
+        }
+        int tmp = a[ i ];
         a[ i ] = a[ min ];
         a[ min ] = tmp;
     }
 }
 
+int main( void )
+{
+    int     a[ 5 ] = { 5, 4, 3, 2, 1 };
+
+    selection( a, 5 );
+    for ( int i = 0; i < 5; ++i )
+        printf( "%d ", a[ i ] );
+    return 0;
+}
