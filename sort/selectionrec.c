cd6d311e043b941e88e168781c9adee7cf320ce5
Yu-Han Lyu <yuhanlyu@tahoe.cs.dartmouth.edu>
2013-05-28 06:58:48 -0400

    Selection sort recursive version

--- sort/selectionrec.c
+++ sort/selectionrec.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 
-void selection( int a[], int start, int n );
-
-int main( void )
+/* Selection sort recursive version, sort the subarray a[start..end-1] */
+void selection( int a[], int start, int end );
+void selection( int a[], int start, int end )
 {
-    int     a[ 5 ] = { 5, 4, 3, 2, 1 }, i;
-
-    selection( a, 0, 5 );
-    for ( i = 0; i < 5; ++i )
-        printf( "%d ", a[ i ] );
-    return 0;
-}
-
-void selection( int a[], int start, int n )
-{
-    int     i, min, tmp;
-
-    if ( start != n - 1 ) {
-        for ( min = start, i = start + 1; i < n; ++i )
+    if ( start != end - 1 ) {
+        int min = start;
+        for ( int i = start + 1; i < end; ++i ) {
             if ( a[ i ] < a[ min ] )
                 min = i;
-        tmp = a[ start ];
+        }
+        int tmp = a[ start ];
         a[ start ] = a[ min ];
         a[ min ] = tmp;
-        selection( a, start + 1, n );
+        selection( a, start + 1, end );
     }
 }
 
+int main( void )
+{
+    int     a[ 5 ] = { 5, 4, 3, 2, 1 };
+
+    selection( a, 0, 5 );
+    for ( int i = 0; i < 5; ++i )
+        printf( "%d ", a[ i ] );
+    return 0;
+}
