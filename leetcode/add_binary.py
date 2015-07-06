27bc343f40f88781d9454df7b82927ace7e4ed13
yuhanlyu <yuhanlyu@gmail.com>
2015-07-06 15:03:37 -0400

    Add Binary

---
+++ leetcode/add_binary.py
@@ -0,0 +1,25 @@
+# Given two binary strings, return their sum (also a binary string).
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution:
+    # @param {string} a
+    # @param {string} b
+    # @return {string}
+    def addBinary(self, a, b):
+        c, aindex, bindex = 0, len(a) - 1, len(b) - 1
+        result = []
+        while aindex >= 0 or bindex>= 0 or c:
+            if aindex >= 0:
+                c += int(a[aindex])
+            if bindex >= 0:
+                c += int(b[bindex])
+            c, reminder = divmod(c, 2)
+            result.insert(0, str(reminder))
+            aindex -= 1
+            bindex -= 1
+        return ''.join(result)
+
+if __name__ == "__main__":
+    solution = Solution()
+    print solution.addBinary("11", "1")
