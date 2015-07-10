dea641ec5ec08dcaa077a978d087320651441fb9
yuhanlyu <yuhanlyu@gmail.com>
2015-07-10 13:03:08 -0400

    Add Binary

--- leetcode/add_binary.py
+++ leetcode/add_binary.py
@@ -2,20 +2,22 @@
 # Time Complexity: O(n)
 # Space Complexity: O(n)
 
+from collections import deque
+
 class Solution:
     # @param {string} a
     # @param {string} b
     # @return {string}
     def addBinary(self, a, b):
         c, aindex, bindex = 0, len(a) - 1, len(b) - 1
-        result = []
+        result = deque()
         while aindex >= 0 or bindex>= 0 or c:
             if aindex >= 0:
                 c += int(a[aindex])
             if bindex >= 0:
                 c += int(b[bindex])
             c, reminder = divmod(c, 2)
-            result.insert(0, str(reminder))
+            result.appendleft(str(reminder))
             aindex -= 1
             bindex -= 1
         return ''.join(result)
