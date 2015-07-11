f8c46e5051ab8e1241e4101896131dea7e67962e
yuhanlyu <yuhanlyu@gmail.com>
2015-07-11 17:26:03 -0400

    Longest Common Prefix

--- leetcode/longest_common_prefix.py
+++ leetcode/longest_common_prefix.py
@@ -1,7 +1,7 @@
 # Write a function to find the longest common prefix string amongst 
 # an array of strings.
 # Time Complexity: O(n)
-# Space Complexity: O(n)
+# Space Complexity: O(output)
 
 class Solution:
     # @param {string[]} strs
