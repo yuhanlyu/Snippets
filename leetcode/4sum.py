bcc01bead7ee17cffe947c1d696bcce0530a68be
yuhanlyu <yuhanlyu@gmail.com>
2015-07-11 19:32:23 -0400

    4Sum

--- leetcode/4sum.py
+++ leetcode/4sum.py
@@ -7,20 +7,17 @@ class Solution:
     # @param {integer} target
     # @return {integer[][]}
     def fourSum(self, nums, target):
-        all_pairs = dict()
-        for i in xrange(len(nums)):
-            for j in xrange(i + 1, len(nums)):
-                if nums[i] + nums[j] not in all_pairs:
-                    all_pairs[nums[i] + nums[j]] = []
-                all_pairs[nums[i] + nums[j]].append([i, j])
-        solutions = set()
-        for key, indices in all_pairs.iteritems():
-            if target - key in all_pairs:
-                solutions |= set((tuple(sorted([nums[i] for i in candidate])) \
-                                  for candidate in \
-                                  (index1 + index2 for index1 in indices \
-                                      for index2 in all_pairs[target - key]) \
-                                  if len(set(candidate)) == 4))
+        all_pairs, solutions = {}, set()
+        nums.sort()
+        for j, i in ((a, b) for a in xrange(len(nums)) for b in xrange(a)):
+            if nums[i] + nums[j] not in all_pairs:
+                all_pairs[nums[i] + nums[j]] = []
+            all_pairs[nums[i] + nums[j]].append((i, j))
+        for j, i in ((a, b) for a in xrange(len(nums)) for b in xrange(a)):
+            key = target - nums[i] - nums[j]
+            if key in all_pairs:
+                solutions |= set(((nums[i], nums[j], nums[k], nums[l])
+                                   for k, l in all_pairs[key] if k > j))
         return [list(solution) for solution in solutions]
                     
 if __name__ == "__main__":
