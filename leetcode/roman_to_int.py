007f19e176e26e3badd9ecd0f5b623ac138b4e22
yuhanlyu <yuhanlyu@gmail.com>
2015-07-13 09:20:58 -0400

    Roman to Integer

--- leetcode/roman_to_int.py
+++ leetcode/roman_to_int.py
@@ -1,20 +1,22 @@
 # Given a roman numeral, convert it to an integer.
 # Input is guaranteed to be within the range from 1 to 3999.
-# Time complexity: O(n) in average (O(n) worst can be achieved by perfect hash)
+# Time complexity: O(n)
 # Space complexity: O(1)
 
 class Solution:
     # @param {string} s
     # @return {integer}
     def romanToInt(self, s):
-        map = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-        previous = result = map[s[-1]]
+        map = [0] * 256
+        map[ord('M')], map[ord('D')], map[ord('C')] = 1000, 500, 100
+        map[ord('L')], map[ord('X')], map[ord('V')] = 50, 10, 5
+        map[ord('I')] = 1
+        previous = result = map[ord(s[-1])]
         for index in xrange(len(s) - 2, -1, -1):
-            val = map[s[index]]
+            val = map[ord(s[index])]
             result += val if val >= previous else -val
             previous = val
         return result
-
 
 if __name__ == "__main__":
     solution = Solution()
