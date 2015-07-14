aae500c84e59f72a7bbac64e88cf7d32e3d04a84
yuhanlyu <yuhanlyu@gmail.com>
2015-07-14 14:37:56 -0400

    Longest Palindromic Substring

---
+++ leetcode/longest_palindromic_substring.py
@@ -0,0 +1,25 @@
+# Given a string S, find the longest palindromic substring in S. 
+# You may assume that the maximum length of S is 1000, and 
+# there exists one unique longest palindromic substring.
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# https://leetcode.com/discuss/28791/manacher-algorithm-in-python-o-n
+
+class Solution:
+    # @param {string} s
+    # @return {string}
+    def longestPalindrome(self, s):
+        T = '#'.join('^{}$'.format(s))
+        P, C, R, max, center = [0] * len(T), 0, 0, 0, 0
+        for i in xrange(1, len(T) - 1):
+            P[i] = (R > i) and min(R - i, P[2 * C - i]) 
+            while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
+                P[i] += 1
+            if P[i] > max: max, center = P[i], i
+            if i + P[i] > R:
+                C, R = i, i + P[i]
+        return s[(center  - max) / 2: (center  + max) / 2]
+
+if __name__ == "__main__":
+    solution = Solution()
+    print solution.longestPalindrome("dabaaba")
