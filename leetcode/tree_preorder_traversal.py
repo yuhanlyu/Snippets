beaee0e40beca44fceba11938a134ee45238cbb4
yuhanlyu <yuhanlyu@gmail.com>
2015-07-19 08:02:09 -0400

    Binary Tree Preorder Traversal

--- leetcode/tree_preorder_traversal.py
+++ leetcode/tree_preorder_traversal.py
@@ -1,6 +1,6 @@
 # Given a binary tree, return the preorder traversal of its nodes' values.
 # Time Complexity: O(n)
-# Space Complexity: O(h), h is the height of the tree
+# Space Complexity: O(1)
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -13,13 +13,33 @@ class Solution:
     # @param {TreeNode} root
     # @return {integer[]}
     def preorderTraversal(self, root):
-        stack, result = [root] if root else [], []
-        while stack:
-            node = stack.pop()
-            result.append(node.val)
-            if node.right: stack.append(node.right)
-            if node.left: stack.append(node.left)
+        result, node = [], root
+        while node:
+            if node.left:
+                pre = node.left
+                while pre.right and pre.right != node:
+                    pre = pre.right
+                if pre.right:
+                    node, pre.right = node.right, None
+                else:
+                    result.append(node.val)
+                    node, pre.right = node.left, node
+            else:
+                result.append(node.val)
+                node = node.right
         return result
+
+#class Solution:
+    # @param {TreeNode} root
+    # @return {integer[]}
+#    def preorderTraversal(self, root):
+#        stack, result = [root] if root else [], []
+#        while stack:
+#            node = stack.pop()
+#            result.append(node.val)
+#            if node.right: stack.append(node.right)
+#            if node.left: stack.append(node.left)
+#        return result
 
 if __name__ == "__main__":
     root = TreeNode(1)
