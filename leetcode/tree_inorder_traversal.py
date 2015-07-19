47ba28aa08e3d4796543cf526483599682d10cc5
yuhanlyu <yuhanlyu@gmail.com>
2015-07-19 07:57:06 -0400

    Binary Tree Inorder Traversal

--- leetcode/tree_inorder_traversal.py
+++ leetcode/tree_inorder_traversal.py
@@ -1,6 +1,6 @@
 # Given a binary tree, return the inorder traversal of its nodes' values.
 # Time Complexity: O(n)
-# Space Complexity: O(h), h is the height of the tree
+# Space Complexity: O(1)
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -13,16 +13,36 @@ class Solution:
     # @param {TreeNode} root
     # @return {integer[]}
     def inorderTraversal(self, root):
-        stack, result, node = [], [], root
-        while stack or node:
-            if node:
-                stack.append(node)
-                node = node.left
+        result, node = [], root
+        while node:
+            if node.left:
+                pre = node.left
+                while pre.right and pre.right != node:
+                    pre = pre.right
+                if pre.right:
+                    result.append(node.val)
+                    node, pre.right = node.right, None
+                else:
+                    pre.right, node = node, node.left
             else:
-                node = stack.pop()
                 result.append(node.val)
                 node = node.right
         return result
+
+#class Solution:
+    # @param {TreeNode} root
+    # @return {integer[]}
+#    def inorderTraversal(self, root):
+#        stack, result, node = [], [], root
+#        while stack or node:
+#            if node:
+#                stack.append(node)
+#                node = node.left
+#            else:
+#                node = stack.pop()
+#                result.append(node.val)
+#                node = node.right
+#        return result
 
 if __name__ == "__main__":
     root = TreeNode(1)
