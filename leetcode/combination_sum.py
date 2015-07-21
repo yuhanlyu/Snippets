8546fb48e6c80c9274143d4daf0483307a26eda8
yuhanlyu <yuhanlyu@gmail.com>
2015-07-21 19:32:19 -0400

    Combination Sum

---
+++ leetcode/combination_sum.py
@@ -0,0 +1,27 @@
+# Given an integer, write a function to determine if it is a power of two.
+# Time Complexity: O(n^n)
+# Space Complexity: O(n^n)
+
+class Solution:
+    # @param {integer[]} candidates
+    # @param {integer} target
+    # @return {integer[][]}
+    def combinationSum(self, candidates, target):
+        cnt, items, DP = {}, [], [[] for _ in xrange(target + 1)]
+        for key in sorted(candidates):
+            cur = 1
+            while cur * key <= target:
+                items.append((cur * key, key, cur))
+                cur *= 2
+        for key, ori, ct in items:
+            for i in xrange(target, key - 1, -1):
+                DP[i].extend((p + [(key, ori, ct)] for p in DP[i - key]))
+            DP[key].append([(key, ori, ct)])
+        for i in xrange(len(DP[target])):
+            DP[target][i] =  \
+                  sum(([key] * count for (_, key, count) in DP[target][i]), [])
+        return DP[target]
+
+if __name__ == "__main__":
+    solution = Solution()
+    print solution.combinationSum([2, 3, 6, 7], 7)
