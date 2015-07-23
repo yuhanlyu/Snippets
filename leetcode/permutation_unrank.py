34ef0a8cf2c852ac6a833b93cb8acef23910fe34
yuhanlyu <yuhanlyu@gmail.com>
2015-07-23 07:22:02 -0400

    Permutation Sequence

--- leetcode/permutation_unrank.py
+++ leetcode/permutation_unrank.py
@@ -1,19 +1,20 @@
 # Given n and k, return the kth permutation sequence.
 # Time Complexity: O(n^2)
-# Space Complexity: O(n^2)
+# Space Complexity: O(n), assuming rotation in place
 
 class Solution:
     # @param {integer} n
     # @param {integer} k
     # @return {string}
     def getPermutation(self, n, k):
-        fac, result = [1] * n, [i + 1 for i in xrange(n)]
+        fac, result = 1, [i + 1 for i in xrange(n)]
         for i in xrange(2, n):
-            fac[i] = fac[i - 1] * i
+            fac *= i
         k -= 1
         for i in xrange(n - 1):
-            (d, k) = divmod(k, fac[n - i - 1])
+            (d, k) = divmod(k, fac)
             if d: result[i:i + d + 1] = [result[i + d]] + result[i:i + d]
+            fac /= (n - 1 - i)
         return ''.join([str(x) for x in result])
 
 if __name__ == "__main__":
