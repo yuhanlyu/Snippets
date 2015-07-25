28197fbec46f5a05df2512f4f4a5d39c5e0f6a86
yuhanlyu <yuhanlyu@gmail.com>
2015-07-25 07:40:15 -0400

    N-Queens II

--- leetcode/n-queensII.py
+++ leetcode/n-queensII.py
@@ -3,21 +3,25 @@
 # distinct solutions.
 
 class Solution:
-    # @param {integer} n
-    # @return {integer}
-    def totalNQueens(self, n):
-        def helper(y, count):
-            if y == n:
-                count[0] += 1
+     # @param {integer} n
+     # @return {integer}
+     def totalNQueens(self, n):
+        mx, d1, d2 = [False] * n, [False] * (2 * n), [False] * (2 * n)
+        stack, solution, result = [(0, 0, False)], [0] * n, 0
+        while stack:
+            y, x, flag = stack.pop()
+            if y == n and flag == False:
+                result += 1
+            elif not flag:
+                if x + 1 < n: stack.append((y, x + 1, False))
+                if not mx[x] and not d1[x + y]  and not d2[n - 1 + x - y]:
+                    mx[x] = d1[x + y] = d2[n - 1 + x - y] = True
+                    solution[y] = x
+                    stack.append((y, x, True))
+                    stack.append((y + 1, 0, False))
             else:
-                for x in xrange(n):
-                    if not mx[x] and not d1[x + y]  and not d2[n - 1 + x - y]:
-                        mx[x] = d1[x + y] = d2[n - 1 + x - y] = True
-                        helper(y + 1, count)
-                        mx[x] = d1[x + y] = d2[n - 1 + x - y] = False
-            return count
-        mx, d1, d2, count = [False] * n, [False] * (2 * n), [False] * (2 * n), 0
-        return helper(0, [0])[0]
+                mx[x] = d1[x + y] = d2[n - 1 + x - y] = False
+        return result
 
 if __name__ == "__main__":
     solution = Solution()
