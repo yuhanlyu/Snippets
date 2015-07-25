9058a399d8bbbc353d8e437183697c105e0f9252
yuhanlyu <yuhanlyu@gmail.com>
2015-07-25 07:35:06 -0400

    N-Queens

--- leetcode/n-queens.py
+++ leetcode/n-queens.py
@@ -7,23 +7,24 @@ class Solution:
     # @param {integer} n
     # @return {string[][]}
     def solveNQueens(self, n):
-        def helper(y, solution, result):
-            if y == n:
-                board = [["."] * n for _ in xrange(n)]
-                for i in xrange(n):
-                    board[i][solution[i]] = "Q"
-                    board[i] = ''.join(board[i])
-                result.append(board)
-            else:
-                for x in xrange(n):
-                    if not mx[x] and not d1[x + y]  and not d2[n - 1 + x - y]:
-                        mx[x] = d1[x + y] = d2[n - 1 + x - y] = True
-                        solution[y] = x
-                        helper(y + 1, solution, result)
-                        mx[x] = d1[x + y] = d2[n - 1 + x - y] = False
-            return result
         mx, d1, d2 = [False] * n, [False] * (2 * n), [False] * (2 * n)
-        return helper(0, [-1] * n, [])
+        stack, solution, result = [(0, 0, False)], [0] * n, []
+        while stack:
+            y, x, flag = stack.pop()
+            if y == n and flag == False:
+                board = [["Q" if solution[i] == j else "." for j in xrange(n)]
+                         for i in xrange(n)]
+                result.append([''.join(item) for item in board])
+            elif not flag:
+                if x + 1 < n: stack.append((y, x + 1, False))
+                if not mx[x] and not d1[x + y]  and not d2[n - 1 + x - y]:
+                    mx[x] = d1[x + y] = d2[n - 1 + x - y] = True
+                    solution[y] = x
+                    stack.append((y, x, True))
+                    stack.append((y + 1, 0, False))
+            else:
+                mx[x] = d1[x + y] = d2[n - 1 + x - y] = False
+        return result
 
 if __name__ == "__main__":
     solution = Solution()
