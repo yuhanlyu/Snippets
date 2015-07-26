58c15c4e1b4e2b0b9b23a101825db937e070398b
yuhanlyu <yuhanlyu@gmail.com>
2015-07-25 21:05:32 -0400

    Combination Sum III

--- leetcode/combination_sumIII.py
+++ leetcode/combination_sumIII.py
@@ -9,17 +9,34 @@ class Solution:
     # @param {integer} n
     # @return {integer[][]}
     def combinationSum3(self, k, n):
-        def plus_one(lists):
-            return ([x + 1 for x in l] for l in lists if l[-1] < 9)
-        DP = [[[] for _ in xrange(k + 1)] for _ in xrange(n + 1)]
-        DP[1][1].append([1])
-        for i in xrange(1, n + 1):
-            for j in xrange(1, min(k + 1, i)):
-                DP[i][j].extend(plus_one(DP[i - j][j]))
-                DP[i][j].extend([1] + p for p in plus_one(DP[i - j][j - 1]))
-        return DP[n][k]
+        def feasible(n, k, l, h):
+            return k * l + k * (k - 1) / 2 <= n <= k * h - k * (k - 1) / 2
+        def min(n, k, l, h):
+            result, nn = [0] * k, n
+            for kk in xrange(k, 0, -1):
+                for x in xrange(h, l - 1, -1):
+                    if feasible(nn - x, kk - 1, l, x - 1):
+                        nn -= x
+                        result[kk - 1], h = x, x - 1
+                        break
+            return result
+        if not feasible(n, k, 1, 9): return []
+        solution = min(n, k, 1, 9)
+        result = [list(solution)]
+        while True:
+            kk, sum = k - 2, solution[k - 1]
+            while kk >= 0 and \
+                not feasible(sum + solution[kk], k - kk, solution[kk] + 1, 9):
+                sum += solution[kk]
+                kk -= 1
+            if kk < 0: break
+            solution[kk] += 1
+            solution[kk + 1:] = min(sum - 1, k - kk - 1, solution[kk] + 1, 9)
+            result.append(list(solution))
+        return result
 
 if __name__ == "__main__":
     solution = Solution()
     print solution.combinationSum3(3, 7)
     print solution.combinationSum3(3, 9)
+    print solution.combinationSum3(3, 2)
