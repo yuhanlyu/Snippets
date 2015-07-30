ea2d58195d219d76a96f9e90a9f82d6a2c236b52
yuhanlyu <yuhanlyu@gmail.com>
2015-07-30 13:36:35 -0400

    Minimum Window Substring

--- leetcode/minimum_window_substring.py
+++ leetcode/minimum_window_substring.py
@@ -8,22 +8,23 @@ class Solution:
     # @param {string} t
     # @return {string}
     def minWindow(self, s, t):
-        need, begin, end, cur, histogram, remain = {}, 0, len(s), 0, {}, set(t)
+        need, begin, end, cur, histogram, remain = {}, 0, len(s), 0, {}, len(t)
         for c in t:
             need[c] = need.get(c, 0) + 1
         for i, c in enumerate(s):
             if c in need:
                 histogram[c] = histogram.get(c, 0) + 1
-                if histogram[c] == need[c] and c in remain: remain.remove(c)
+                if histogram[c] <= need[c]: remain -= 1
             while not remain:
                 if  i - cur < end - begin: begin, end = cur, i
                 if s[cur] in need:
                     histogram[s[cur]] -= 1
                     if histogram[s[cur]] < need[s[cur]]:
-                        remain.add(s[cur])
+                        remain += 1
                 cur += 1
         return s[begin:end + 1] if (end - begin) < len(s) else ""
 
 if __name__ == "__main__":
     solution = Solution()
-    print solution.minWindow("ADOBECODEBANC", "ABC")
+    print solution.minWindow("aa", "aa")
+    #print solution.minWindow("ADOBECODEBANC", "ABC")
