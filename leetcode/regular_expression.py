6230ec85fada996088a023f2a7f68b467aae2d81
yuhanlyu <yuhanlyu@gmail.com>
2015-08-05 09:48:24 -0400

    Regular Expression Matching

--- leetcode/regular_expression.py
+++ leetcode/regular_expression.py
@@ -7,22 +7,25 @@ class Solution:
     # @param {string} p
     # @return {boolean}
     def isMatch(self, s, p):
-        def match(p, c, state):
+        def match(p, c, state, cache):
             if state == len(p): return []
             if p[state] == c or p[state] == '.':
                 if state < len(p) - 1 and p[state + 1] == '*':
-                    return epsilon(p, state)
-                return epsilon(p, state + 1)
+                    return epsilon(p, state, cache)
+                return epsilon(p, state + 1, cache)
             return []
-        def epsilon(p, state):
+        def epsilon(p, state, cache):
+            if state in cache: return cache[state]
             result = set([state])
             while state < len(p) - 1 and p[state + 1] == '*':
                 state += 2
                 result.add(state)
+            cache[state] = result
             return result
-        states = epsilon(p, 0)
+        cache = {}
+        states = epsilon(p, 0, cache)
         for c in s:
-            states = set([x for y in states for x in match(p, c, y)])
+            states = set([x for y in states for x in match(p, c, y, cache)])
         return len(p) in states
 
 if __name__ == "__main__":
