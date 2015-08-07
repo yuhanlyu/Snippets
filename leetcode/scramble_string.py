2f6fac14905e3cbb00e00c03754395a367ea2f4b
yuhanlyu <yuhanlyu@gmail.com>
2015-08-07 13:34:13 -0400

    Scramble String

--- leetcode/scramble_string.py
+++ leetcode/scramble_string.py
@@ -1,6 +1,6 @@
 # Given a string s1, we may represent it as a binary tree by partitioning it 
 # to two non-empty substrings recursively.
-# Time Complexity: O(n^4)
+# Time Complexity: O(n^4) in average
 # Space Complexity: O(n^3)
 
 class Solution:
@@ -11,15 +11,14 @@ class Solution:
         L = [[set([j for j in xrange(len(s2)) if s1[i] == s2[j]])
                      for i in xrange(len(s1))]]
         for k in xrange(1, len(s1)):
-            L.append([])
+            L.append([set() for _ in xrange(len(s1) - k)])
             for i in xrange(len(s1) - k):
-                L[-1].append(set())
                 for m in xrange(k):
                     for j in L[m][i]:
                         if j + m + 1 in L[k - m - 1][i + m + 1]: 
-                            L[-1][-1].add(j)
+                            L[k][i].add(j)
                         if j - k + m in L[k - m - 1][i + m + 1]:
-                            L[-1][-1].add(j - k + m)
+                            L[k][i].add(j - k + m)
         return len(L[-1][-1]) > 0
 
 if __name__ == "__main__":
