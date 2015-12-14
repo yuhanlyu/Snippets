2eb6099e531b2b3009bca89af3bb9094f119bfeb
yuhanlyu <yuhanlyu@gmail.com>
2015-12-14 16:22:07 -0500

    Hidden Digits

---
+++ codeeval/hidden_digits.rb
@@ -0,0 +1,11 @@
+File.open(ARGV[0]).each_line do |line|
+    result = ""
+    line.split("").each do |c|
+        if c =~ /[0-9]/
+            result << c
+        elsif c =~ /[a-j]/
+            result << (c.ord - 97).to_s
+        end
+    end
+    puts result.empty? ? "NONE" : result
+end
