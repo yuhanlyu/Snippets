b6b37c7c0238e94aa6d28f3fa40a92edb245d9a2
yuhanlyu <yuhanlyu@gmail.com>
2015-12-14 18:39:55 -0500

    Self Describing Number

--- codeeval/self_describing_number.rb
+++ codeeval/self_describing_number.rb
@@ -3,7 +3,7 @@ File.open(ARGV[0]).each_line do |line|
     digits = line.chomp.chars.map(&:to_i)
     if digits.length <= 10
         counts = digits.each_with_object(Array.new(10, 0)) {|d, o| o[d] += 1}
-        result = (0..digits.length - 1).none? { |i| counts[i] != digits[i] }
+        result = (0..digits.length - 1).all? { |i| counts[i] == digits[i] }
     end
     puts result ? "1" : "0"
 end
