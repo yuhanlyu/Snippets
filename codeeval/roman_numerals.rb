8460b23028ac66bf836dda961cbfec05734da724
yuhanlyu <yuhanlyu@gmail.com>
2015-12-16 14:59:52 -0500

    Roman Numerals

---
+++ codeeval/roman_numerals.rb
@@ -0,0 +1,9 @@
+M = ["", "M", "MM", "MMM"]
+C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+File.open(ARGV[0]).each_line do |line|
+    num = line.to_i
+    puts M[num / 1000] + C[(num % 1000) / 100] + 
+         X[(num % 100) / 10] + I[num % 10]
+end
