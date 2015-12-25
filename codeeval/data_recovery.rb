fb15e41e5dfa69fcb23d6b818e98675d7303864f
yuhanlyu <yuhanlyu@gmail.com>
2015-12-25 16:04:02 -0500

    Data Recovery

--- codeeval/data_recovery.rb
+++ codeeval/data_recovery.rb
@@ -2,6 +2,5 @@ File.open(ARGV[0]).each_line do |line|
     s, p = line.split(";").map(&:split)
     p = p.map {|i| i.to_i - 1}
     p += [(0...s.length).find {|i| !p.include?(i)}]
-    puts (0...s.length).each_with_object(Array.new(s.length)) {|i,r|
-          r[p[i]] = s[i]}.join(" ")
+    puts p.zip(s).sort.map(&:last).join(" ")
 end
