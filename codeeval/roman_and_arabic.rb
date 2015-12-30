29151041c8e6a8a829321d9d2acc99558f2d6ad5
yuhanlyu <yuhanlyu@gmail.com>
2015-12-30 15:04:48 -0500

    Roman And Arabic

--- codeeval/roman_and_arabic.rb
+++ codeeval/roman_and_arabic.rb
@@ -3,7 +3,7 @@ map = {'I' => 1, 'V' => 5, 'X' => 10, 'L' => 50,
 File.open(ARGV[0]).each_line do |line|
     d = (0...line.size - 1).step(2).map {|i| line[i].to_i}
     r = (1...line.size - 1).step(2).map {|i| line[i]}
-    puts r.each_with_index.each_cons(2).inject(0) {|sum, ((p, pi), (n, ni))|
-        sum + d[pi] * map[p] * (map[n] > map[p] ? -1 : 1)
+    puts r.zip(d).each_cons(2).inject(0) {|sum, ((p, pd), (n, _))|
+        sum + pd * map[p] * (map[n] > map[p] ? -1 : 1)
     } + d[-1] * map[r[-1]]
 end
