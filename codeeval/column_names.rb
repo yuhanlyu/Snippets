3696745aba38b7dee679a8b678b29ac01b57138a
yuhanlyu <yuhanlyu@gmail.com>
2016-01-02 16:29:36 -0500

    Column Names

--- codeeval/column_names.rb
+++ codeeval/column_names.rb
@@ -1,8 +1,8 @@
 File.open(ARGV[0]).each_line do |line|
-    n, result = line.to_i, []
+    n, ans = line.to_i, []
     while n > 0
         n, remainder = (n - 1).divmod(26)
-        result << ("A".ord + remainder).chr
+        ans.unshift(("A".ord + remainder).chr)
     end
-    puts result.reverse.join
+    puts ans.join
 end
