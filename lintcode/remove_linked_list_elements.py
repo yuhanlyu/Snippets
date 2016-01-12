49889921020f3fd253022edaf6694a5871975f55
yuhanlyu <yuhanlyu@gmail.com>
2016-01-12 18:38:01 -0500

    Remove Linked List Elements

---
+++ lintcode/remove_linked_list_elements.py
@@ -0,0 +1,31 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @param val, an integer
+    # @return a ListNode
+    def removeElements(self, head, val):
+        dummy = ListNode(0)
+        dummy.next = head
+        current = dummy
+        while current.next:
+            if current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        return dummy.next
+
+if __name__ == "__main__":
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(6)
+    head.next.next.next = ListNode(3)
+    head.next.next.next.next = ListNode(4)
+    head.next.next.next.next.next = ListNode(5)
+    head.next.next.next.next.next.next = ListNode(6)
+    solution = Solution()
+    head = solution.removeElements(head, 6)
+    print head.val, head.next.val, head.next.next.val
