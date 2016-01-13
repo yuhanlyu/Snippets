48129dfb9c60525d487b98763e272029d70571e2
yuhanlyu <yuhanlyu@gmail.com>
2016-01-13 16:01:46 -0500

    Maximum Subarray

---
+++ leetcode/maximum_subarray.js
@@ -0,0 +1,20 @@
+/* Find the contiguous subarray within an array (containing at least one number)
+ * which has the largest sum.
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    var result = nums[0], cur = 0           
+    for (var i = 0; i < nums.length; ++i) {
+        cur += nums[i]
+        result = Math.max(result, cur)
+        if (cur < 0)
+            cur = 0
+    }
+    return result
+};
