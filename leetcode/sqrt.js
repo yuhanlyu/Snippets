42d8e0e58cf107d156b6760e747112246511d5f6
yuhanlyu <yuhanlyu@gmail.com>
2016-01-13 13:13:07 -0500

    Sqrt(x)

---
+++ leetcode/sqrt.js
@@ -0,0 +1,17 @@
+/* Implement int sqrt(int x).  Compute and return the square root of x.
+ * Time Complexity: O(lg lg n)
+ * Space Complexity: O(1)
+ */
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    var n = 1, nn = 3           
+    while (Math.abs(n - nn) > 1) {
+        nn = n
+        n = Math.floor((n + Math.floor(x / n)) / 2)
+    }
+    return n * n <= x ? n : n - 1
+};
