f68a78bd4b48af2ff8db25f7a3d013643db795b3
yuhanlyu <yuhanlyu@gmail.com>
2016-01-13 13:03:27 -0500

    Add Two Numbers

--- leetcode/add_two_numbers.js
+++ leetcode/add_two_numbers.js
@@ -1,8 +1,9 @@
-# You are given two linked lists representing two non-negative numbers. 
-# The digits are stored in reverse order and each of their nodes contain 
-# a single digit. Add the two numbers and return it as a linked list.
-# Time Complexity: O(n)
-# Space Complexity: O(1)
+/* You are given two linked lists representing two non-negative numbers. 
+ * The digits are stored in reverse order and each of their nodes contain 
+ * a single digit. Add the two numbers and return it as a linked list.
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
