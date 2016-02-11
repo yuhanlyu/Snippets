ff4bdc9a2036eb8ee00905b775f49c3f4a8a93bb
yuhanlyu <yuhanlyu@gmail.com>
2016-02-11 15:42:40 -0500

    Search a 2D Matrix

---
+++ leetcode/search_2d_matrix.js
@@ -0,0 +1,40 @@
+/* Write an efficient algorithm that searches for a value in an m x n matrix. 
+ * This matrix has the following properties:
+ * Integers in each row are sorted from left to right.
+ * The first integer of each row is greater than the last integer of the 
+ * previous row.
+ * Time Complexity: O(lg n)
+ * Space Complexity: O(1)
+ */
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    left = 0
+    right = matrix.length - 1
+    while (left <= right) {
+        mid = left + Math.floor((right - left) / 2)
+        if (matrix[mid][0] < target)
+            left = mid + 1
+        else
+            right = mid - 1
+    }
+    if (left < matrix.length && matrix[left][0] == target)
+        return true
+    if (left == 0)
+        return false
+    index = left - 1
+    left = 0
+    right = matrix[index].length - 1
+    while (left <= right) {
+        mid = left + Math.floor((right - left) / 2)
+        if (matrix[index][mid] < target)
+            left = mid + 1
+        else
+            right = mid - 1
+    }
+    return left < matrix[index].length && matrix[index][left] == target
+};
