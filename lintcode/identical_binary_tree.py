599ef0421c8ad94cbc7ae2e2c97f47ad4ccdb53e
yuhanlyu <yuhanlyu@gmail.com>
2016-02-13 18:40:31 -0500

    Identical Binary Tree

--- lintcode/identical_binary_tree.py
+++ lintcode/identical_binary_tree.py
@@ -11,8 +11,7 @@ class Solution:
     @return true if they are identical, or false.
     """
     def isIdentical(self, a, b):
-        if not a and not b: return True
-        if not a or not b: return False
-        if a.val != b.val: return False
-        return self.isIdentical(a.left, b.left) and \
-               self.isIdentical(a.right, b.right)
+        if not a or not b:
+            return not a and not b
+        return a.val == b.val and self.isIdentical(a.left, b.left) \
+                              and self.isIdentical(a.right, b.right)
