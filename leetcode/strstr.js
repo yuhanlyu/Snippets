07ee3cbb65cdc7167448d5ef522fbb769b0bab1b
yuhanlyu <yuhanlyu@gmail.com>
2016-02-14 11:58:46 -0500

    Implement strStr()

---
+++ leetcode/strstr.js
@@ -0,0 +1,27 @@
+/* Returns the index of the first occurrence of needle in haystack, 
+ * or -1 if needle is not part of haystack.
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (needle == "")
+        return 0
+    failure = [-1] 
+    for (var i = 1; i <= needle.length; ++i) {
+        for (var pos = failure[i - 1]; pos != -1 && needle[pos] != needle[i-1];)
+            pos = failure[pos]
+        failure[i] = pos + 1
+    }
+    for (var ti = 0, pi = 0; ti < haystack.length; ++ti) {
+        while (pi != -1 && haystack[ti] != needle[pi])
+            pi = failure[pi]
+        if (++pi == needle.length)
+            return ti + 1 - needle.length
+    }
+    return -1
+};
