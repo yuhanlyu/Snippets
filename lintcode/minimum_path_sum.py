87cd1f400560771197b77b7bb4cc0e67ff069624
yuhanlyu <yuhanlyu@gmail.com>
2016-02-13 19:16:44 -0500

    Minimum Path Sum

---
+++ lintcode/minimum_path_sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    """
+    @param grid: a list of lists of integers.
+    @return: An integer, minimizes the sum of all numbers along its path
+    """
+    def minPathSum(self, grid):
+        for i in xrange(len(grid)):
+            for j in xrange(len(grid[i])):
+                if i > 0 and j > 0:
+                    grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+                elif i > 0:
+                    grid[i][j] += grid[i - 1][j]
+                elif j > 0:
+                    grid[i][j] += grid[i][j - 1]
+        return grid[-1][-1]
