025979e97c7219077ff761a8550eee6b0dae38a7
yuhanlyu <yuhanlyu@gmail.com>
2016-02-14 09:30:36 -0500

    Fizz Buzz

---
+++ lintcode/fizz_buzz.py
@@ -0,0 +1,19 @@
+class Solution:
+    """
+    @param n: An integer as description
+    @return: A list of strings.
+    For example, if n = 7, your code should return
+        ["1", "2", "fizz", "4", "buzz", "fizz", "7"]
+    """
+    def fizzBuzz(self, n):
+        results = []
+        for i in range(1, n+1):
+            if i % 15 == 0:
+                results.append("fizz buzz")
+            elif i % 5 == 0:
+                results.append("buzz")
+            elif i % 3 == 0:
+                results.append("fizz")
+            else:
+                results.append(str(i))
+        return results
