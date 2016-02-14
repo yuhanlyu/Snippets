1889cdaf2b8a4b37d8d726a60e1d52260614fa0e
yuhanlyu <yuhanlyu@gmail.com>
2016-02-14 18:38:17 -0500

    A + B

---
+++ lintcode/a+b.py
@@ -0,0 +1,8 @@
+class Solution:
+    """
+    @param a: The first integer
+    @param b: The second integer
+    @return:  The sum of a and b
+    """
+    def aplusb(self, a, b):
+        return a + b
