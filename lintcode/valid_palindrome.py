6fc16fa0583f14959402875fd96bfaa3a850f8e4
yuhanlyu <yuhanlyu@gmail.com>
2016-02-14 19:16:45 -0500

    Valid Palindrome

---
+++ lintcode/valid_palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param {string} s A string
+    # @return {boolean} Whether the string is a valid palindrome
+    def isPalindrome(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while right > left and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
