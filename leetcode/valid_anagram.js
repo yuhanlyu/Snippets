5752ea09a3f12b6f69a63801177d677db4c2637a
yuhanlyu <yuhanlyu@gmail.com>
2016-02-15 12:42:30 -0500

    Valid Anagram

---
+++ leetcode/valid_anagram.js
@@ -0,0 +1,21 @@
+/* Given two strings s and t, write a function to determine if t is an anagram 
+ * of s.
+ * Time Complexity: O(n lg n)
+ * Space Complexity: O(1)
+ */
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length != t.length)
+        return false
+    s = s.split("").sort()
+    t = t.split("").sort()
+    for (var i = 0; i < s.length; ++i)
+        if (s[i] != t[i])
+            return false
+    return true
+};
