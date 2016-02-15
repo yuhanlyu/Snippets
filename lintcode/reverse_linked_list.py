b0f5facfcd598f61afb1fd1c479b5a50e60d9681
yuhanlyu <yuhanlyu@gmail.com>
2016-02-15 16:23:46 -0500

    Reverse Linked List

---
+++ lintcode/reverse_linked_list.py
@@ -0,0 +1,20 @@
+"""
+Definition of ListNode
+
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+class Solution:
+    """
+    @param head: The first node of the linked list.
+    @return: You should return the head of the reversed linked list. 
+                  Reverse it in-place.
+    """
+    def reverse(self, head):
+        prev = None
+        while head:
+            prev, head.next, head = head, prev, head.next
+        return prev
