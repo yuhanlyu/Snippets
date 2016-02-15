945280714045d67f1395cd8ba1e0c4db1c35a983
yuhanlyu <yuhanlyu@gmail.com>
2016-02-14 19:31:45 -0500

    Valid Palindrome

---
+++ leetcode/valid_palindrome.js
@@ -0,0 +1,21 @@
+/* Given a string, determine if it is a palindrome, considering only 
+ * alphanumeric characters and ignoring cases.
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    for (var left = 0, right = s.length - 1; left < right; ++left, --right) {
+        while (left < right && !/^[a-z0-9]+$/i.test(s[left]))
+            ++left
+        while (left < right && !/^[a-z0-9]+$/i.test(s[right]))
+            --right
+        if (s[left].toLowerCase() != s[right].toLowerCase())
+            return false
+    }    
+    return true
+};
