8488126710152fbfce5c3b6fb121ff62fecae5e0
yuhanlyu <yuhanlyu@gmail.com>
2016-02-15 16:43:35 -0500

    Delete Node in the Middle of Singly Linked List

---
+++ lintcode/delete_node_in_the_middle_of_singly_linked_list.py
@@ -0,0 +1,13 @@
+"""
+Definition of ListNode
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+class Solution:
+    # @param node: the node in the list should be deleted
+    # @return: nothing
+    def deleteNode(self, node):
+        node.val, node.next = node.next.val, node.next.next
