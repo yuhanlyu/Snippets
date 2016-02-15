94bb4e12720a94756d38de36a42f9c9217258efd
yuhanlyu <yuhanlyu@gmail.com>
2016-02-15 16:34:35 -0500

    Merge Two Sorted Lists

---
+++ lintcode/merge_two_sorted_lists.py
@@ -0,0 +1,25 @@
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+class Solution:
+    """
+    @param two ListNodes
+    @return a ListNode
+    """
+    def mergeTwoLists(self, l1, l2):
+        dummy = ListNode(0)
+        current = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+        current.next = l1 if l1 else l2
+        return dummy.next
