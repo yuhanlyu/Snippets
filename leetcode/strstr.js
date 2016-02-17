149b8ea4e729e82fd893061e633dfc79b400fc0e
yuhanlyu <yuhanlyu@gmail.com>
2016-02-17 18:44:37 -0500

    Implement strStr()

--- leetcode/strstr.js
+++ leetcode/strstr.js
@@ -9,18 +9,18 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    if (needle == "")
+    if (needle === "")
         return 0
-    failure = [-1] 
+    var failure = [-1]
     for (var i = 1; i <= needle.length; ++i) {
-        for (var pos = failure[i - 1]; pos != -1 && needle[pos] != needle[i-1];)
+        for (var pos = failure[i-1]; pos != -1 && needle[pos] !== needle[i-1];)
             pos = failure[pos]
         failure[i] = pos + 1
     }
     for (var ti = 0, pi = 0; ti < haystack.length; ++ti) {
-        while (pi != -1 && haystack[ti] != needle[pi])
+        while (pi != -1 && haystack[ti] !== needle[pi])
             pi = failure[pi]
-        if (++pi == needle.length)
+        if (++pi === needle.length)
             return ti + 1 - needle.length
     }
     return -1
