9038a910da928bfc25bf456e1e2ead89e86dca95
yuhanlyu <yuhanlyu@gmail.com>
2016-02-17 10:29:32 -0500

    Trapping Rain Water

---
+++ leetcode/trapping_rain_water.js
@@ -0,0 +1,26 @@
+/* Given n non-negative integers representing an elevation map where the width 
+ * of each bar is 1, compute how much water it is able to trap after raining.
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    result = 0
+    for (var h = 0, left = 0, right = height.length - 1; left <= right; ) {
+        h = Math.max(h, Math.min(height[left], height[right]))
+        if (height[left] < height[right]) {
+            if (height[left] < h)
+                result += h - height[left]
+            ++left
+        } else {
+            if (height[right] < h)
+                result += h - height[right]
+            --right
+        }
+    }
+    return result
+};
