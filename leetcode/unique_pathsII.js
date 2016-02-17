991bab6366d1583aeb880d5345123b9357d9056a
yuhanlyu <yuhanlyu@gmail.com>
2016-02-17 18:52:35 -0500

    Unique Paths II

--- leetcode/unique_pathsII.js
+++ leetcode/unique_pathsII.js
@@ -11,15 +11,15 @@
  * @return {number}
  */
 var uniquePathsWithObstacles = function(obstacleGrid) {
-    for (i = 0; i < obstacleGrid.length; ++i) {
-        for (j = 0; j < obstacleGrid[i].length; ++j) {
-            if (obstacleGrid[i][j] == 1)
+    for (var i = 0; i < obstacleGrid.length; ++i) {
+        for (var j = 0; j < obstacleGrid[i].length; ++j) {
+            if (obstacleGrid[i][j] === 1)
                 obstacleGrid[i][j] = 0
-            else if (i == 0 && j == 0)
+            else if (i === 0 && j === 0)
                 obstacleGrid[i][j] = 1
-            else if (i == 0)
+            else if (i === 0)
                 obstacleGrid[i][j] = obstacleGrid[i][j - 1]
-            else if (j == 0)
+            else if (j === 0)
                 obstacleGrid[i][j] = obstacleGrid[i - 1][j]
             else
                 obstacleGrid[i][j] = obstacleGrid[i - 1][j]
