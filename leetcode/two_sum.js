b2443d17c024b7fba8188342290cbf851c0b3ff1
yuhanlyu <yuhanlyu@gmail.com>
2016-02-17 11:12:36 -0500

    Two Sum

---
+++ leetcode/two_sum.js
@@ -0,0 +1,19 @@
+/* Given an array of integers, find two numbers such that they add up to a 
+ * specific target number.
+ * Time Complexity: O(n) in average
+ * Space Complexity: O(n)
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    elements = {}
+    for (var i = 0; i < nums.length; ++i)
+        elements[nums[i]] = i
+    for (var i = 0; i < nums.length; ++i)
+        if (target - nums[i] in elements && i != elements[target - nums[i]])
+            return [i, elements[target - nums[i]]]
+};
