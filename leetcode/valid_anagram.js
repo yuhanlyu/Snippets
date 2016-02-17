16bfc1d05c15df1cbbcf03883a786824093a70cc
yuhanlyu <yuhanlyu@gmail.com>
2016-02-17 18:53:27 -0500

    Valid Anagram

--- leetcode/valid_anagram.js
+++ leetcode/valid_anagram.js
@@ -10,12 +10,12 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if (s.length != t.length)
+    if (s.length !== t.length)
         return false
     s = s.split("").sort()
     t = t.split("").sort()
     for (var i = 0; i < s.length; ++i)
-        if (s[i] != t[i])
+        if (s[i] !== t[i])
             return false
     return true
 };
