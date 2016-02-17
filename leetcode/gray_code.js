0fb01e7e5a8a20c90cd09ba6f4deb63b906796d4
yuhanlyu <yuhanlyu@gmail.com>
2016-02-17 09:34:13 -0500

    Gray Code

---
+++ leetcode/gray_code.js
@@ -0,0 +1,17 @@
+/* Given a non-negative integer n representing the total number of bits in 
+ * the code, print the sequence of gray code. 
+ * A gray code sequence must begin with 0.
+ * Time Complexity: O(2^n)
+ * Space Complexity: O(2^n)
+ */
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var grayCode = function(n) {
+    result = []
+    for (var x = 0; x < Math.pow(2, n); ++x)
+        result.push((x >> 1) ^ x)
+    return result
+};
