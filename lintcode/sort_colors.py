680be358b60cc02342df29cac463c9d144236b84
yuhanlyu <yuhanlyu@gmail.com>
2016-02-18 13:35:53 -0500

    Sort Colors

---
+++ lintcode/sort_colors.py
@@ -0,0 +1,17 @@
+class Solution:
+    """
+    @param nums: A list of integer which is 0, 1 or 2 
+    @return: nothing
+    """
+    def sortColors(self, nums):
+        left, mid, right = 0, 0, len(nums) - 1
+        while mid <= right:
+            if nums[mid] == 0:
+                nums[mid], nums[left] = nums[left], nums[mid]
+                left += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1
