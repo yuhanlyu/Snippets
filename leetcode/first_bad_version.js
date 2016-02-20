4240de4e02328ab2030fd7d09d9c57f96a7f5d3a
yuhanlyu <yuhanlyu@gmail.com>
2016-02-20 11:23:18 -0500

    First Bad Version

---
+++ leetcode/first_bad_version.js
@@ -0,0 +1,43 @@
+/* You are a product manager and currently leading a team to develop a new 
+ * product. Unfortunately, the latest version of your product fails the quality 
+ * check. Since each version is developed based on the previous version, 
+ * all the versions after a bad version are also bad.
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the 
+ * first bad one, which causes all the following ones to be bad.
+ * You are given an API bool isBadVersion(version) which will return whether 
+ * version is bad. Implement a function to find the first bad version. 
+ * You should minimize the number of calls to the API.
+ * Time Complexity: O(lg n)
+ * Space Complexity: O(1)
+ */
+
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        for (var low = 1, high = n; low <= high; ) {
+            var mid = low + Math.floor((high - low) / 2);
+            if (!isBadVersion(mid))
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return low;
+    };
+};
