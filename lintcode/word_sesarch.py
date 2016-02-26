67a757af51416e35208dea60b39952f390814ae4
yuhanlyu <yuhanlyu@gmail.com>
2016-02-26 12:33:35 -0500

    Word Search

---
+++ lintcode/word_sesarch.py
@@ -0,0 +1,22 @@
+class Solution:
+    # @param board, a list of lists of 1 length string
+    # @param word, a string
+    # @return a boolean
+    def exist(self, board, word):
+        def helper(row, col, visited, word, level):
+            if row < 0 or row >= len(board) or col < 0 \
+            or col >= len(board[row]) or visited[row][col] \
+            or level >= len(word) or board[row][col] != word[level]:  
+                return False
+            if level == len(word) - 1: return True
+            visited[row][col] = True
+            for drow, dcol in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                if helper(row + drow, col + dcol, visited, word, level + 1):
+                    return True
+            visited[row][col] = False
+            return False
+        visited = [[False] * len(board[0]) for _ in xrange(len(board))]
+        for i in xrange(len(board)):
+            for j in xrange(len(board[i])):
+                if helper(i, j, visited, word, 0): return True
+        return False
