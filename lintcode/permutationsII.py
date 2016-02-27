bca3c8d5c639c7aa99602b6bc8711e5e8b66d8e5
yuhanlyu <yuhanlyu@gmail.com>
2016-02-27 07:26:47 -0500

    Permutations II

---
+++ lintcode/permutationsII.py
@@ -0,0 +1,21 @@
+class Solution:
+    """
+    @param nums: A list of integers.
+    @return: A list of unique permutations.
+    """
+    def permuteUnique(self, nums):
+        nums.sort()
+        result = [list(nums)]
+        while True:
+            i = len(nums) - 2
+            while i >= 0 and nums[i] >= nums[i + 1]:
+                i -= 1
+            if i < 0: break
+            j = len(nums) - 1
+            while nums[i] >= nums[j]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+            left, right = i + 1, len(nums) - 1
+            nums[i + 1:] = nums[-1:i:-1]
+            result.append(list(nums))
+        return result
