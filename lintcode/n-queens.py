7b758b8d13ae24742e43da8f044732b2a19341ce
yuhanlyu <yuhanlyu@gmail.com>
2016-02-27 07:57:10 -0500

    N-Queens

--- lintcode/n-queens.py
+++ lintcode/n-queens.py
@@ -18,8 +18,7 @@ class Solution:
                 if not mx[x] and not d1[x + y]  and not d2[n - 1 + x - y]:
                     mx[x] = d1[x + y] = d2[n - 1 + x - y] = True
                     solution[y] = x
-                    stack.append((y, x, True))
-                    stack.append((y + 1, 0, False))
+                    stack.extend([(y, x, True), (y + 1, 0, False)])
             else:
                 mx[x] = d1[x + y] = d2[n - 1 + x - y] = False
         return result
