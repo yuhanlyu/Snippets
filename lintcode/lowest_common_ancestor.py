e458815ccff8d548f0f421444613232ea49f94d3
yuhanlyu <yuhanlyu@gmail.com>
2016-02-28 17:17:44 -0500

    Lowest Common Ancestor

---
+++ lintcode/lowest_common_ancestor.py
@@ -0,0 +1,20 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+import copy
+class Solution:
+    """
+    @param root: The root of the binary search tree.
+    @param A and B: two nodes in a Binary.
+    @return: Return the least common ancestor(LCA) of the two nodes.
+    """ 
+    def lowestCommonAncestor(self, root, A, B):
+        if root in (None, A, B): return root
+        left = self.lowestCommonAncestor(root.left, A, B)
+        right = self.lowestCommonAncestor(root.right, A, B)
+        if left and right: return root
+        return left if left else right
