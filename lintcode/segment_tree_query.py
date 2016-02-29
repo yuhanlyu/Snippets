6f77fc4869790ba070556803f30aef6e970d002a
yuhanlyu <yuhanlyu@gmail.com>
2016-02-28 19:21:34 -0500

    Segment Tree Query

---
+++ lintcode/segment_tree_query.py
@@ -0,0 +1,21 @@
+"""
+Definition of SegmentTreeNode:
+class SegmentTreeNode:
+    def __init__(self, start, end, max):
+        self.start, self.end, self.max = start, end, max
+        self.left, self.right = None, None
+"""
+
+class Solution: 
+    # @param root, start, end: The root of segment tree and 
+    #                          an segment / interval
+    # @return: The maximum number in the interval [start, end]
+    def query(self, root, start, end):
+        if not root or root.start > end or root.end < start: return -2 ** 31
+        if start <= root.start and root.end <= end: return root.max
+        left, right = -2 ** 31, -2 ** 31
+        if root.left.start <= end:
+            left = self.query(root.left, start, end)
+        if root.right.end >= start:
+            right = self.query(root.right, start, end)
+        return max(left, right)
