53bfc257bb274182062571410de796a5bdc66fcd
yuhanlyu <yuhanlyu@gmail.com>
2016-02-29 15:27:14 -0500

    Minimum Window Substring

--- leetcode/minimum_window_substring.js
+++ leetcode/minimum_window_substring.js
@@ -12,18 +12,19 @@
 var minWindow = function(s, t) {
     var need = new Map();
     for (var c of t.split("")) {
-        if (!(c in need))
-            need[c] = 0;
-        ++need[c];
+        if (!need.has(c))
+            need.set(c, 0);
+        need.set(c, need.get(c) + 1);
     }
     var histogram = new Map(), remain = t.length;
     var begin = 0, end = s.length, cur = 0;
     for (var i = 0; i < s.length; ++i) {
         c = s.charAt(i);
-        if (c in need) {
-            if (!(c in histogram))
-                histogram[c] = 0;
-            if (++histogram[c] <= need[c])
+        if (need.has(c)) {
+            if (!histogram.has(c))
+                histogram.set(c, 0);
+            histogram.set(c, histogram.get(c) + 1);
+            if (histogram.get(c) <= need.get(c))
                 --remain;
         }
         for (; remain === 0; ++cur) {
@@ -31,9 +32,11 @@ var minWindow = function(s, t) {
                 begin = cur;
                 end = i;
             }
-            if (s.charAt(cur) in need 
-            && --histogram[s.charAt(cur)] < need[s.charAt(cur)])
+            if (need.has(s.charAt(cur))) {
+                histogram.set(s.charAt(cur), histogram.get(s.charAt(cur)) - 1);
+                if (histogram.get(s.charAt(cur)) < need.get(s.charAt(cur)))
                     ++remain;
+            }
         }
     }
     return end - begin < s.length ? s.substring(begin, end + 1) : "";
