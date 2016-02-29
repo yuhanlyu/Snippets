cae250443a16c2f7fd398acdfc8c588a11d11654
yuhanlyu <yuhanlyu@gmail.com>
2016-02-29 15:20:03 -0500

    Longest Consecutive Sequence

--- leetcode/longest_consecutive_sequence.js
+++ leetcode/longest_consecutive_sequence.js
@@ -11,13 +11,13 @@
 var longestConsecutive = function(nums) {
     var b = new Map(), result = 0;
     for (var num of nums) {
-        if (!(num in b)) {
-            var left = num - 1 in b ? b[num - 1] : num;
-            var right = num + 1 in b ? b[num + 1] : num;
+        if (!b.has(num)) {
+            var left = b.has(num - 1) ? b.get(num - 1) : num;
+            var right = b.has(num + 1) ? b.get(num + 1) : num;
             result = Math.max(result, right - left + 1);
-            b[num] = num;
-            b[left] = right;
-            b[right] = left;
+            b.set(num, num);
+            b.set(left, right);
+            b.set(right, left);
         }
     }
     return result;
