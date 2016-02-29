e6f0fc46986b607e4134369b21e4a2d71714b712
yuhanlyu <yuhanlyu@gmail.com>
2016-02-28 19:26:32 -0500

    Segment Tree Query II

---
+++ lintcode/segment_tree_queryII.py
@@ -0,0 +1,16 @@
+"""
+Definition of SegmentTreeNode:
+class SegmentTreeNode:
+    def __init__(self, start, end, count):
+        self.start, self.end, self.count = start, end, count
+        self.left, self.right = None, None
+"""
+
+class Solution: 
+    # @param root, start, end: The root of segment tree and 
+    #                          an segment / interval
+    # @return: The count number in the interval [start, end] 
+    def query(self, root, start, end):
+        if not root or root.start > end or root.end < start: return 0
+        if start <= root.start and root.end <= end: return root.count
+        return self.query(root.left, start, end) + self.query(root.right, start, end)
