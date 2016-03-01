d186ef90a8b042561074f25ac3c0b3a7d479398b
yuhanlyu <yuhanlyu@gmail.com>
2016-03-01 13:14:43 -0500

    Partition Array

--- lintcode/partition_array.py
+++ lintcode/partition_array.py
@@ -6,7 +6,7 @@ class Solution:
     """
     def partitionArray(self, nums, k):
         left, right = 0, len(nums) - 1
-        while left <= right:
+        while left < right:
             while left <= right and nums[left] < k:
                 left += 1
             while left <= right and nums[right] >= k:
