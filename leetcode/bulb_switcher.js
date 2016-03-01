89cc38325a86864c3e7f52cd590d4c2d3c67821c
yuhanlyu <yuhanlyu@gmail.com>
2016-03-01 13:38:02 -0500

    Bulb Switcher

---
+++ leetcode/bulb_switcher.js
@@ -0,0 +1,17 @@
+/* There are n bulbs that are initially off. You first turn on all the bulbs. 
+ * Then, you turn off every second bulb. On the third round, you toggle every 
+ * third bulb (turning on if it's off or turning off if it's on). For the ith 
+ * round, you toggle every i bulb. For the nth round, you only toggle the last 
+ * bulb. Find how many bulbs are on after n rounds.
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bulbSwitch = function(n) {
+    return Math.floor(Math.sqrt(n));
+};
