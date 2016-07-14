c0578a88224348c6d6ed94aee9731797ff7748b2
yuhanlyu <yuhanlyu@gmail.com>
2016-07-13 21:24:43 -0700

    Clean up

--- experiments/fibonacci/fib.c
+++ experiments/fibonacci/fib.c
@@ -1,7 +1,6 @@
 #include "fib.h"
 
-unsigned doubling(unsigned n)
-{
+unsigned doubling(unsigned n) {
     if (n == 0) return 0;
     if (n == 1) return 1;
     unsigned j, nn = n;
@@ -20,8 +19,7 @@ unsigned doubling(unsigned n)
     return s2;
 }
 
-unsigned tumble(unsigned n) 
-{
+unsigned tumble(unsigned n) {
     unsigned even = 0, odd = 1, i;
 
     // even is the i-th fibonacci number
@@ -33,11 +31,9 @@ unsigned tumble(unsigned n)
     return i == n ? even : odd;
 }
 
-unsigned iterative(unsigned n)
-{
+unsigned iterative(unsigned n) {
     unsigned first = 0, second = 1;
-    for (unsigned k = 0; k < n; ++k)
-    {
+    for (unsigned k = 0; k < n; ++k) {
         unsigned t = first + second;
         first = second;
         second = t;
@@ -45,11 +41,10 @@ unsigned iterative(unsigned n)
     return first;
 }
 
-unsigned qmatrix(unsigned n)
-{
+unsigned qmatrix(unsigned n) {
     unsigned a = 1, b = 0, c = 0, d = 1;
     for (--n; n > 0; n >>= 1) {
-        if (n & 1){
+        if (n & 1) {
             unsigned t = d * (b + a) + c * b;
             a = d * b + c * a;
             b = t;
