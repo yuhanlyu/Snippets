f4c3d37694bd367fbd3dedb819398576b83b82c6
yuhanlyu <yuhanlyu@gmail.com>
2016-07-15 05:48:28 -0700

    Test

--- experiments/binarysearch/binary_search_test.cc
+++ experiments/binarysearch/binary_search_test.cc
@@ -10,7 +10,6 @@ class SearchTest : public ::testing::Test {
         for (int32_t i = 0; i < size; ++i)
             test[i] = i;
     }
-  public:
     static constexpr int32_t size = 10000000;
     int32_t test[size];
 };
