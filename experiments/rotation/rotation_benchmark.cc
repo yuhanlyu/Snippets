ce951831aad4fe4983a65e7d6f07649c87c83a99
yuhanlyu <yuhanlyu@gmail.com>
2016-07-15 05:24:33 -0700

    Benchmark

--- experiments/rotation/rotation_benchmark.cc
+++ experiments/rotation/rotation_benchmark.cc
@@ -4,49 +4,51 @@
 
 namespace {
 
-const int32_t range = 1000000U;
-int32_t temp[range];
-int32_t shift = range / 17;
+class RotationBenchmark : public benchmark::Fixture {
+  public:
+    void SetUp(const ::benchmark::State& state) {
+        for (int32_t i = 0; i < range; ++i)
+            temp[i] = i;
+    }
+    static constexpr int32_t range = 1000000;
+    static constexpr int32_t shift = range / 17;
+    int32_t temp[range];
+};
 
-static void BM_JugglingBentley(benchmark::State& state) {
+BENCHMARK_F(RotationBenchmark, JugglingBentley)(benchmark::State& state) {
     while (state.KeepRunning()) {
         juggling_bentley(temp, range, shift);
     }
     benchmark::DoNotOptimize(temp[0]);
 }
-BENCHMARK(BM_JugglingBentley);
 
-static void BM_JugglingShene(benchmark::State& state) {
+BENCHMARK_F(RotationBenchmark, JugglingShene)(benchmark::State& state) {
     while (state.KeepRunning()) {
         juggling_shene(temp, range, shift);
     }
     benchmark::DoNotOptimize(temp[0]);
 }
-BENCHMARK(BM_JugglingShene);
 
-static void BM_RotateReverse(benchmark::State& state) {
+BENCHMARK_F(RotationBenchmark, RotateReverse)(benchmark::State& state) {
     while (state.KeepRunning()) {
         rotate_reverse(temp, range, shift);
     }
     benchmark::DoNotOptimize(temp[0]);
 }
-BENCHMARK(BM_RotateReverse);
 
-static void BM_BlockSwapShene(benchmark::State& state) {
+BENCHMARK_F(RotationBenchmark, BlockSwapShene)(benchmark::State& state) {
     while (state.KeepRunning()) {
         block_swap_shene(temp, range, shift);
     }
     benchmark::DoNotOptimize(temp[0]);
 }
-BENCHMARK(BM_BlockSwapShene);
 
-static void BM_BlockSwapGries(benchmark::State& state) {
+BENCHMARK_F(RotationBenchmark, BlockSwapGries)(benchmark::State& state) {
     while (state.KeepRunning()) {
         block_swap_gries(temp, range, shift);
     }
     benchmark::DoNotOptimize(temp[0]);
 }
-BENCHMARK(BM_BlockSwapGries);
 
 }
-BENCHMARK_MAIN()
+BENCHMARK_MAIN();
