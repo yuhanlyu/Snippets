f2b8228ae590dc8ea1a4aa7164762c5b74184ee0
yuhanlyu <yuhanlyu@gmail.com>
2016-07-15 05:37:12 -0700

    Test

--- experiments/Josephus/josephus_test.cc
+++ experiments/Josephus/josephus_test.cc
@@ -4,36 +4,33 @@
 
 namespace {
 
+static constexpr int32_t n = 2000000;
+
 TEST(JosephusTest, ValidateShamsBaragh) {
-    const int32_t n = 2000000;
     for (int32_t m = 100000; m < n; m += 100000) {
         EXPECT_EQ(shams_baragh(n, m), taocp_k(n, m, n));
     }
 }
 
 TEST(JosephusTest, ValidateWoodhousea) {
-    const int32_t n = 2000000;
     for (int32_t m = 100000; m < n; m += 100000) {
         EXPECT_EQ(woodhousea(n, m), taocp_k(n, m, n));
     }
 }
 
 TEST(JosephusTest, ValidateGelgi) {
-    const int32_t n = 2000000;
     for (int32_t m = 100000; m < n; m += 100000) {
         EXPECT_EQ(gelgi(n, m), taocp_k(n, m, n));
     }
 }
 
 TEST(JosephusTest, ValidateGelgiImprove) {
-    const int32_t n = 2000000;
     for (int32_t m = 100000; m < n; m += 100000) {
         EXPECT_EQ(gelgi_improve(n, m), taocp_k(n, m, n));
     }
 }
 
 TEST(JosephusTest, ValidateGelgiTAOCP) {
-    const int32_t n = 2000000;
     for (int32_t m = 100000; m < n; m += 100000) {
         EXPECT_EQ(taocp(n, m), taocp_k(n, m, n));
     }
