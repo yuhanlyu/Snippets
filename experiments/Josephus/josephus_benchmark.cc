1252caee5e24e3320576460e076d488ef314d39f
yuhanlyu <yuhanlyu@gmail.com>
2016-07-15 18:59:25 -0700

    Benchmark

--- experiments/Josephus/josephus_benchmark.cc
+++ experiments/Josephus/josephus_benchmark.cc
@@ -5,49 +5,49 @@
 namespace {
 
 static constexpr int n = 2000000;
-static constexpr int m = 100000;
+static constexpr int m = 97;
 
 static void BM_ShamsBaragh(benchmark::State& state) {
     while (state.KeepRunning()) {
-        benchmark::DoNotOptimize(shams_baragh(n, m));
+        benchmark::DoNotOptimize(shams_baragh(n, state.range_x()));
     }
 }
-BENCHMARK(BM_ShamsBaragh);
+BENCHMARK(BM_ShamsBaragh)->RangeMultiplier(m)->Range(2, n);
 
 static void BM_Woodhousea(benchmark::State& state) {
     while (state.KeepRunning()) {
-        benchmark::DoNotOptimize(woodhousea(n, m));
+        benchmark::DoNotOptimize(woodhousea(n, state.range_x()));
     }
 }
-BENCHMARK(BM_Woodhousea);
+BENCHMARK(BM_Woodhousea)->RangeMultiplier(m)->Range(2, n);
 
 static void BM_Gelgi(benchmark::State& state) {
     while (state.KeepRunning()) {
-        benchmark::DoNotOptimize(gelgi(n, m));
+        benchmark::DoNotOptimize(gelgi(n, state.range_x()));
     }
 }
-BENCHMARK(BM_Gelgi);
+BENCHMARK(BM_Gelgi)->RangeMultiplier(m)->Range(2, n);
 
 static void BM_GelgiImprove(benchmark::State& state) {
     while (state.KeepRunning()) {
-        benchmark::DoNotOptimize(gelgi_improve(n, m));
+        benchmark::DoNotOptimize(gelgi_improve(n, state.range_x()));
     }
 }
-BENCHMARK(BM_GelgiImprove);
+BENCHMARK(BM_GelgiImprove)->RangeMultiplier(m)->Range(2, n);
 
 static void BM_TAOCP(benchmark::State& state) {
     while (state.KeepRunning()) {
-        benchmark::DoNotOptimize(taocp(n, m));
+        benchmark::DoNotOptimize(taocp(n, state.range_x()));
     }
 }
-BENCHMARK(BM_TAOCP);
+BENCHMARK(BM_TAOCP)->RangeMultiplier(m)->Range(2, n);
 
 static void BM_TAOCPK(benchmark::State& state) {
     while (state.KeepRunning()) {
-        benchmark::DoNotOptimize(taocp_k(n, m, n));
+        benchmark::DoNotOptimize(taocp_k(n, state.range_x(), n));
     }
 }
-BENCHMARK(BM_TAOCPK);
+BENCHMARK(BM_TAOCPK)->RangeMultiplier(m)->Range(2, n);
 
 }
 BENCHMARK_MAIN();
