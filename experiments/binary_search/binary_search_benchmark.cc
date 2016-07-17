55c12b186c369105b892613b9161431e3f0dff73
yuhanlyu <yuhanlyu@gmail.com>
2016-07-17 10:55:42 -0700

    Benchmark

--- experiments/binary_search/binary_search_benchmark.cc
+++ experiments/binary_search/binary_search_benchmark.cc
@@ -18,9 +18,8 @@ class SearchBenchmark : public benchmark::Fixture {
         }
         std::sort(test, test + size);
     }
-    static constexpr int32_t size  = 1000000;
+    static constexpr int32_t size  = 1 << 19;
     static constexpr int32_t max = INT32_MAX;
-    static constexpr int32_t m = max / size;
     std::uniform_int_distribution<int32_t> distribution{0, max};
     std::default_random_engine generator;
     int32_t test[size];
@@ -33,22 +32,22 @@ BENCHMARK_F(SearchBenchmark, Random)(benchmark::State& state) {
 }
 
 BENCHMARK_F(SearchBenchmark, BinarySearch)(benchmark::State& state) {
-    for (int32_t r = distribution(generator); state.KeepRunning(); 
-         r = distribution(generator)) {
+    for (int32_t r = 0; state.KeepRunning(); ) {
+        r = distribution(generator);
         benchmark::DoNotOptimize(binary_search(test, size, r));
     }
 }
 
 BENCHMARK_F(SearchBenchmark, BiasedSearch)(benchmark::State& state) {
-    for (int32_t r = distribution(generator); state.KeepRunning(); 
-         r = distribution(generator)) {
+    for (int32_t r = 0; state.KeepRunning(); ) {
+        r = distribution(generator);
         benchmark::DoNotOptimize(biased_search(test, size, r));
     }
 }
 
 BENCHMARK_F(SearchBenchmark, STLSearch)(benchmark::State& state) {
-    for (int32_t r = distribution(generator); state.KeepRunning(); 
-         r = distribution(generator)) {
+    for (int32_t r = 0; state.KeepRunning(); ) {
+        r = distribution(generator);
         benchmark::DoNotOptimize(std::lower_bound(test, test + size, r));
     }
 }
@@ -60,8 +59,8 @@ int compare(const void *p, const void *q) {
 }
 
 BENCHMARK_F(SearchBenchmark, BSearch)(benchmark::State& state) {
-    for (int32_t r = distribution(generator); state.KeepRunning(); 
-         r = distribution(generator)) {
+    for (int32_t r = 0; state.KeepRunning(); ) {
+        r = distribution(generator);
         benchmark::DoNotOptimize(bsearch(&r, test, size, sizeof(int32_t),
                                          compare));
     }
